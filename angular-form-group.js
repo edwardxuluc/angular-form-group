67a1a784e1e2832e02b4dfec1fc86544df5c3416
Edward paulino xuluc chulim <edward.xuluc@creatibot.com>
2019-01-16 16:47:34 -0600

    agregado de ; al final de codigo

--- angular-form-group.js
+++ angular-form-group.js
@@ -5,17 +5,18 @@ angular.module('angular.form.group', [])
 .directive('formGroup', function( $compile ){
     return {
         restrict : 'A',
-        link : function ( scope, element ){
+        link : function ( scope, element, attrs ){
 
             var element   = angular.element( element );
             var formName  = element.parents('[ng-form]').length ? element.parents('[ng-form]').attr('ng-form') : element.parents('form').attr('name');
-            var inputName = element.find('.form-control').attr('name');
-
+            var inputName = attrs.name || element.find('.form-control').attr('name');
+            
             if( formName && inputName ){
+                inputName       = inputName.replace(/[\{\}\[\]\(\)']+/g, '');
                 var new_element = $compile('<div ng-class="{\'has-error\':' + formName + '.' + inputName + '.$invalid && !' + formName + '.' + inputName + '.$pristine, \'has-success\':' + formName + '.' + inputName + '.$valid && !' + formName + '.' + inputName + '.$pristine}"></div>')( scope );
                 new_element.append( element.contents() );
-                element.html( new_element ); 
+                element.html( new_element );
             }
         }
     }
-})
+});
